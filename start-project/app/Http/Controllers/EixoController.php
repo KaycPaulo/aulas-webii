8c61820831e90b37dd6a1149936e097cc9edde40
KaycPaulo <127959968+KaycPaulo@users.noreply.github.com>
2024-08-14 17:48:20 -0300

    commit update
    isaque qui fez

--- start-project/app/Http/Controllers/EixoController.php
+++ start-project/app/Http/Controllers/EixoController.php
@@ -2,26 +2,109 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Eixo;
 use Illuminate\Http\Request;
+use App\Models\Eixo;
 
 class EixoController extends Controller
 {
-    public function index(){
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
         $data = Eixo::all();
         return view('eixo.index', compact(['data']));
     }
 
-    public function create(){
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
         return view('eixo.create');
     }
 
-    public function store(Request $request){
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
         $eixo = new Eixo();
         $eixo->name = $request->name;
         $eixo->description = $request->description;
         $eixo->save();
 
         return redirect()->route('eixo.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id){
+
+        $eixo = Eixo::find($id);
+    
+        if(isset($eixo)){
+            return view('eixo.show', compact('eixo'));
+        }
+
+        return "<h1>EIXO NÃO ENCONTRADO</h1>";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $eixo = Eixo::find($id);
+
+        if(isset($eixo)){
+            return view('eixo.edit', compact('eixo'));
+        }
+
+        return "<h1>EIXO NÃO ENCONTRADO</h1>";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $eixo = Eixo::find($id);
+
+        if(isset($eixo)){
+            $eixo->name = $request->name;
+            $eixo->description = $request->description;
+            $eixo->save();
+            return redirect()->route('eixo.index');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- start-project/resources/views/eixo/index.blade.php
+++ start-project/resources/views/eixo/index.blade.php
@@ -13,6 +13,7 @@
         <th>ID</th>
         <th>NAME</th>
         <th>DESCRIPTION</th>
+        <th>ACTIONS</th>
     </thead>
 
     <tbody>
@@ -21,6 +22,19 @@
             <td>{{$item->id}}</td>
             <td>{{$item->name}}</td>
             <td>{{$item->description}}</td>
+            <td>
+                <a href="{{route('eixo.show', $item->id)}}" class="btn btn-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
+                    </svg>
+                </a>
+                <a href="{{route('eixo.edit', $item->id)}}" class="btn btn-success">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                    </svg>
+                </a>
+            </td>
         </tr>
         @endforeach
     </tbody>
